d5f46c17cbe02201ac8d23ec4395451bc96915c8
DTGuy <tguy5837@gmail.com>
2022-09-19 16:42:26 -0400

    Generated HTML file

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js')
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+const profileDataArgs = process.argv.slice(2);
 
-    console.log('===============')
+const [name, github] = profileDataArgs;
 
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
 
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the results.')
+});
+
+
+// HELPFUL NOTES FROM MODULE 9.2.3
+// const printProfileData = profileDataArr => {
+//     // >>>>> This...
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('===============')
+
+//     // >>>>> Is the same as this...
+//     profileDataArr.forEach((profileItem) => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
