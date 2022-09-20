29f093a6883f788bfb642403900ee874e771f181
DTGuy <tguy5837@gmail.com>
2022-09-20 17:11:01 -0400

    added inquirer to project

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js')
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js')
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+//     console.log('Portfolio complete! Check out index.html to see the results.')
+// });
 
-    console.log('Portfolio complete! Check out index.html to see the results.')
-});
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ]).then(answers => console.log(answers));
+
 
 
 // HELPFUL NOTES FROM MODULE 9.2.3
